7551624150df4d7904ca12900c52a0b97b30f8f5
Mishra-Abhishek9644 <mishraabhishek9644@gmail.com>
2025-10-25 09:47:35 +0530

    DB connection Learned

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+// import express from "express" 
+// const app = express()                          
+
+// ( async () =>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", ()=>{
+//         console.log("errr",error);
+//         throw error
+//        })
+
+//        app.listen(process.env.PORT,() => {
+//         console.log(`app is listening on port ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.error("errror",error)
+//         throw err
+//     }
+// })()
